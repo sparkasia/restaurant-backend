a1629f5d4dc982d486ce7017251bbee7067aa84c
khalilahmad3212 <arzishjani1613@gmail.com>
2024-08-30 23:32:40 +0500

    add: health route

--- index.js
+++ index.js
@@ -20,6 +20,11 @@ app.use(express.json());
 app.use('/api/restaurants', restaurantRoutes);
 app.use('/api/auth', authRoutes);
 
+// headlth route
+app.get('/', (req, res) => {
+  res.send('Server is running');
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
